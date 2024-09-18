d5448ed3f02cca4c5b3090a762ea05b18f68d3ad
ianza <ianza@Ians_PC.dhcp.wustl.edu>
2024-09-18 14:17:19 -0500

    Studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,29 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number for n:");
+		int n = in.nextInt(); 
+		
+		int[ ] primeNums = new int[n];
+
+		boolean isPrime = true;
+		
+		for (int i = 0; i < primeNums.length; i++) {
+			primeNums[i] = i + 1;
+			
+			
+			
+			
+			
+		System.out.println( primeNums[i] );
+				
+				
+		}
+	}
+
+}
